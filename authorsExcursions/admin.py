6e89494cffffc87b105feb28e14de16e42b36960
avc105 <avc105@tpu.ru>
2025-04-18 23:48:55 +0700

    done

--- authorsExcursions/admin.py
+++ authorsExcursions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from authorsExcursions.models import User, RegisteredUser, Guide, Administrator, Tour, Cart, CartItem, Order
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(RegisteredUser)
+admin.site.register(Guide)
+admin.site.register(Administrator)
+admin.site.register(Tour)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+
+# каталог, фидбек
